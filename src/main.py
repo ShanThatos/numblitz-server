dff024623179c920e459fcc5d1265359aee00eec
ShanThatos <shanthkoka@gmail.com>
2024-11-07 22:39:00 -0600

    fixing ip route

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-import socket
+import os
+
+from subprocess import check_output
+
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -11,14 +14,15 @@ app = FastAPI(debug=ENVIRONMENT == "dev")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(api_router)
 
-@app.get("/ip")
-def get_ip():
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    return {
-        "hostname": hostname,
-        "ip_address": ip_address
-    }
+if os.name != "nt":
+    @app.get("/ip")
+    def get_ip():
+        hostname = check_output("hostname", shell=True, text=True).strip()
+        ip_address = check_output("hostname -I", shell=True, text=True).strip()
+        return {
+            "hostname": hostname,
+            "ip_address": ip_address
+        }
 
 if ENVIRONMENT == "dev":
     app.include_router(admin_router)
