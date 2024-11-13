2d2ec5cca869ee70789f368b60fdc127fe207078
ShanThatos <shanthkoka@gmail.com>
2024-11-13 02:53:33 -0600

    removing temporary server info route

--- src/main.py
+++ src/main.py
@@ -1,8 +1,4 @@
-import os
-
-from subprocess import check_output
-
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -13,16 +9,6 @@ from routes.admin import router as admin_router
 app = FastAPI(debug=ENVIRONMENT == "dev")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(api_router)
-
-if os.name != "nt":
-    @app.get("/ip")
-    def get_ip():
-        hostname = check_output("hostname", shell=True, text=True).strip()
-        ip_address = check_output("hostname -I", shell=True, text=True).strip()
-        return {
-            "hostname": hostname,
-            "ip_address": ip_address
-        }
 
 if ENVIRONMENT == "dev":
     app.include_router(admin_router)
